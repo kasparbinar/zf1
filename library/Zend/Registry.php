d93a29c4328c2f5ea90038e4445e12b130c75d65
kasparbinar <kaspar-@mail.ru>
2022-08-26 15:05:43 +0200

    back

--- library/Zend/Registry.php
+++ library/Zend/Registry.php
@@ -203,7 +203,7 @@ class Zend_Registry extends ArrayObject
      */
     public function offsetExists($index)
     {
-        return property_exists($this, $index);
+        return array_key_exists($index, $this);
     }
 
 }
